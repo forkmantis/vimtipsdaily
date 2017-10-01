8c9db19a3e2ce00aa787bca8416336770ea4ea33
Will Killian <user.email>
2017-10-01 10:09:28 -0500

    Add tasks from section IV on registers.

--- bot.js
+++ bot.js
@@ -28,7 +28,24 @@ var phraseArray = [ "Practice {command}s with the aw motion. (i.e. daw to delete
                     "Use ( and ) to jump to the beginning and end of sentences.",
                     "Use gf to jump to the file name under the cursor. https://vimhelp.appspot.com/editing.txt.html#gf",
                     "Use :changes to see the change list.  Use g; and g, to jump to the locations of the changes.",
-                    "Use m{capital letter} to make a global mark. Close and reopen VIm, and press '{capital letter} to open the file w/ the global mark."
+                    "Use m{capital letter} to make a global mark. Close and reopen VIm, and press '{capital letter} to open the file w/ the global mark.",
+                    "Use the \"0p to put from the yank register.  Useful when you have to edit with deletions, changes, etc after yanking.",
+                    "Delete using the \"_d{motion} command to delete without saving a copy, preserving your default register.",
+                    "Use the \"+ register to interact with the system clipboard. I.e., \"+p to put system clipboard into your current buffer.",
+                    "Put from the \"% register to put the name of the current file.",
+                    "Put from the \"# register to put the name of the alternate file (the most recent other file edited in the current buffer).",
+                    "Put from the \": register to put the most recently executed EX command.",
+                    "Put from the \"/ register to put the last search pattern used.",
+                    "In insert mode, put the contents of the default register with <c-r>\".",
+                    "Use gP and gp to put before or after the current line, and keep the cursor on the previous line.",
+                    "Use :reg to view the contents of all registers, or :reg{register} to view the contents of one.",
+                    "Use 10@a to play the macro saved in register a 10 times.",
+                    "Use :normal @a to play the macro saved in register a on every line in the selection.",
+                    "Use qA to append keystrokes to the macro in register a.",
+                    "Use :edit! to revert all changes to the first buffer in the argument list",
+                    "Use :wall to save changes to all files in the buffer list.",
+                    "Use 'var += 1' in a macro to increment a number.  Useful for numbering lists, etc.",
+                    "Edit a macro by putting it's register into your buffer, editing, then yanking it back to its regsiter. \"ap, [edit], \"add."
                     ];
 function chooseRandom(myArray) {
   return myArray[Math.floor(Math.random() * myArray.length)];
