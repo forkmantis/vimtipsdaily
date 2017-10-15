6e2b5c8a3ed8aa504437a97630ccf1743336f5f8
Will Killian <user.email>
2017-10-15 09:40:48 -0500

    Add comments to denote where the tips came from.

--- bot.js
+++ bot.js
@@ -6,7 +6,9 @@ var Bot = new Twit({
  access_token: process.env.BOT_ACCESS_TOKEN,
  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
-var phraseArray = [ "Practice {command}s with the aw motion. (i.e. daw to delete the word under your cursor)",
+var phraseArray = [ 
+                    /* Tips from Practical VIm Part I. Modes */
+                    "Practice {command}s with the aw motion. (i.e. daw to delete the word under your cursor)",
                     "From Insert mode, we can switch to Normal mode for one operation by pressing < C-o >",
                     "Use <c-a> to increment the next number.  {number}<c-a> adds {number} to the next number.",
                     "Use <c-x> to decrement the next number.  {number}<c-x> subtracts {number} from the next number.",
@@ -19,9 +21,11 @@ var phraseArray = [ "Practice {command}s with the aw motion. (i.e. daw to delete
                     "Use the argdo: command on a visual selection to perform the same operation across multiple lines",
                     "Use '{range}: normal {stuff}' to edit multiple lines in a selection",
                     "Use :ls command to list buffers in memory",
+                    /* Tips from Practical VIm Part II. Files */
                     "In directory view (netrw), create a new file with '%'",
                     "In directory view (netrw), create a new directory with 'd'",
                     "Use ':write !{cmd}' to pipe buffer as input into the specified command",
+                    /* Tips from Practical VIm Part III. Getting Around Faster */
                     "Use 'e' to move to the end of the next word, and 'ge' to the end of the previous word",
                     "Use a /search as the motion for some other command, like delete (d/search<C-R>)",
                     "Use `` to jump to snap back to where you were before your last jump (or '' to jump to the last line)",
@@ -30,6 +34,7 @@ var phraseArray = [ "Practice {command}s with the aw motion. (i.e. daw to delete
                     "Use :changes to see the change list.  Use g; and g, to jump to the locations of the changes.",
                     "Use m{capital letter} to make a global mark. Close and reopen VIm, and press '{capital letter} to open the file w/ the global mark.",
                     "Use the \"0p to put from the yank register.  Useful when you have to edit with deletions, changes, etc after yanking.",
+                    /* Tips from Practical VIm Part IV. Registers */
                     "Delete using the \"_d{motion} command to delete without saving a copy, preserving your default register.",
                     "Use the \"+ register to interact with the system clipboard. I.e., \"+p to put system clipboard into your current buffer.",
                     "Put from the \"% register to put the name of the current file.",
