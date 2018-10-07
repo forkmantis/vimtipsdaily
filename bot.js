abe8c2895f6493e2377ac25b49e4613c96c60a5b
Will Killian <github.user@forkmantis.com>
2018-10-06 21:18:55 -0500

    Add tip on how to open VIm directly to the first search term in a file.

--- bot.js
+++ bot.js
@@ -71,6 +71,7 @@ var phraseArray = [
                     "Use yHP to yank from the current line to the top of the screen and PUT above the top of the screen.",
                     "Use :2,3co. to copy lines 2 and 3 and PUT it below the current line. Use :help :co for more info", 
                     "Use '&' to repeat your last substitution on the current line.  Use 'g&' to repeat the last substitution on all lines.",
+                    "Open VIm directly to the first occurrence of a search term with `vim +/searchterm filename`",
                     ];
 function chooseRandom(myArray) {
   return myArray[Math.floor(Math.random() * myArray.length)];
