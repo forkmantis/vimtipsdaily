573b40988ba02e28a1f68d7ecdf3e99e3bcf1548
Will Killian <github.user@forkmantis.com>
2019-01-09 18:26:42 -0600

    Add tip about using relative ranges

--- bot.js
+++ bot.js
@@ -73,7 +73,8 @@ var phraseArray = [
                     "Use '&' to repeat your last substitution on the current line.  Use 'g&' to repeat the last substitution on all lines.",
                     "Open VIm directly to the first occurrence of a search term with `vim +/searchterm filename`",
                     "Repeat your most recent search/replace on the current line by pressing &",
-                    ];
+                    "Use a relative range to perform a substitution.  i.e. -3,-1s#a#X#g to replace 'a' w/ 'X' from from 3 lines to 1 line above the cursor.",
+                ];
 function chooseRandom(myArray) {
   return myArray[Math.floor(Math.random() * myArray.length)];
 }
