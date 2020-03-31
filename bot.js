e65e301d4a67b1cbeaa572ea97cf7d1cc4368d9c
Will Killian <github.user@forkmantis.com>
2020-03-31 09:01:01 -0500

    Clarified relative range tip, thx to feedback from @Arlo_Clarke

--- bot.js
+++ bot.js
@@ -73,7 +73,7 @@ var phraseArray = [
                     "Use '&' to repeat your last substitution on the current line.  Use 'g&' to repeat the last substitution on all lines.",
                     "Open VIm directly to the first occurrence of a search term with `vim +/searchterm filename`",
                     "Repeat your most recent search/replace on the current line by pressing &",
-                    "Use a relative range to perform a substitution.  i.e. -3,-1s#a#X#g to replace 'a' w/ 'X' from from 3 lines to 1 line above the cursor.",
+                    "Use a relative range to perform a substitution.  i.e. -3,-1s#a#X#g to replace all instances of 'a' w/ 'X' from from 3 lines to 1 line above the cursor.",
                     "Execute a command on a series of lines matching a pattern, i.e. ':g/aaa/y A' to yank all lines matching 'aaa' into the register. https://vim.fandom.com/wiki/Power_of_g",
                     "Execute a command on a series of lines not matching a pattern, i.e. ':v/aaa/d' to delete all lines not containing 'aaa'. https://vim.fandom.com/wiki/Power_of_g",
                     "Select a column of numbers, and use g<C-a> to increment them as a set",
